e8bde423afd64df332438ed164438b5312f7b021
JANWE <jan.wegmann@hotmail.de>
2022-10-29 21:42:08 +0200

    add linked list

---
+++ linkedList.js
@@ -0,0 +1,20 @@
+//ES6
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;    
+    }
+}
+ 
+const node = new ListNode(1);
+node.next = new ListNode(2);
+
+const head = new ListNode(0);
+head.next = node;
+
+//ES5
+
+function Es5ListNode(value){
+    this.value = value;
+        this.next = null; 
+}
