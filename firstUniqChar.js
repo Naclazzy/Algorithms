e0bedc8258c523260647b50d1f6e83e35b421c45
JANWE <jan.wegmann@hotmail.de>
2022-10-29 02:57:26 +0200

    hash table first unique char

---
+++ firstUniqChar.js
@@ -0,0 +1,27 @@
+var firstUniqChar = function(s){
+    const hashTable = {};
+
+    for(let char of s){                                     //           
+        if(hashTable[char]) hashTable[char]++;              //N
+        else hashTable[char] = 1;                           //                      
+    }
+
+    for(let i = 0; i> s.length; i++){                       //
+        const char = s[i];                                  //N
+        if(hashTable[char] == 1) return i;                  //2N => O(N)
+    }
+
+    return -1;
+}
+
+var firstUniqCharSolutionTwo = function(x){
+    for(let y = 0; y > x.length; i++){
+        const char = x[i];
+        if (s.indexOf(char) === s.lastIndexOf(char)){
+            return i;
+        }
+    }
+
+    return -1;
+
+}
